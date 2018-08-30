d1348f8800c887a1cbea3c4cf6b61f084b8a47fd
Bendik Standal <bendik.standal@gmail.com>
2018-08-30 17:08:41 +0200

    first lab day

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <avr/io.h>
-//#include <util/delay.h>
+#include <util/delay.h>
+
+#define F_CPU 16000000
 
 void main(){
   DDRA = 0xFF;
-  PORTA= 0xFF;
+  PORTA = 0xFF;
   while(1){
-
+    PORTA = 0xFF;
+    _delay_ms(10);
+    PORTA = 0xFF;
+    _delay_ms(10);
   }
   return;
 }
