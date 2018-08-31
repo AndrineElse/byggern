563b4b418b00a2fcb16355fb791128f7c781d8c5
Bendik Standal <bendik.standal@gmail.com>
2018-08-31 13:35:43 +0200

    first lab task done

--- main.c
+++ main.c
@@ -1,9 +1,23 @@
+#define F_CPU 5000000
+
 #include <avr/io.h>
 #include <util/delay.h>
+#include "UARTdriver.h"
 
-#define F_CPU 16000000
+#define FOSC 1843200// Clock Speed
+#define BAUD 9600
+#define MYUBRR F_CPU/16/BAUD-1
 
 void main(){
+
+  USART_Init ( MYUBRR );
+  while(1){
+    // USART_Transmit('A');
+    _delay_ms(1000);
+    printf("TEST");
+  }
+  /*
+  Firkantpuls
   DDRA = 0xFF;
   PORTA = 0;
   while(1){
@@ -11,6 +25,6 @@ void main(){
     _delay_ms(10);
     PORTA = 0;
     _delay_ms(10);
-  }
+  }*/
   return;
 }
