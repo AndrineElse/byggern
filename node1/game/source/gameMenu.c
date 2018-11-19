528a64b2204046dc05e49afffd1abf70486374ee
theakbe <theak.bergh@gmail.com>
2018-11-19 22:44:15 +0100

    changed print_highscore

--- node1/game/source/gameMenu.c
+++ node1/game/source/gameMenu.c
@@ -214,10 +214,10 @@ void printNodeUsingBuffer(volatile struct Node* node, uint8_t selectedOption){
     print_highscore_node(game_highscore_update()[0], game_highscore_update()[1], game_highscore_update()[2], game_highscore_update()[3]);
     for (int i = 0; i < node->numOptions; i++){
       if (i == selectedOption){
-        OLED_buffer_print_line(node->options[i], i+3, 1);
+        OLED_buffer_print_line(node->options[i], i+4, 1);
       }
       else {
-        OLED_buffer_print_line(node->options[i],i+3,0);
+        OLED_buffer_print_line(node->options[i],i+4,0);
       }
     }
   }
@@ -317,6 +317,84 @@ void game_username_select(uint8_t selectedOption){
 }
 
 void print_highscore_node(uint8_t place, uint8_t username, uint8_t score_H, uint8_t score_L){
+  uint16_t currentByte = place*128;
+
+  // PLACE
+  char* number;
+  switch(place){
+    case 1:
+      number = "1. ";
+      break;
+    case 2:
+      number = "2. ";
+      break;
+    case 3:
+      number = "3. ";
+      break;
+  }
+  uint16_t a = 0;
+  while(number[a]) {
+    uint8_t currentChar = (uint8_t)number[a];
+    if(currentChar < 32){
+      continue;
+    }
+    uint8_t font_table_index = currentChar - 32;
+  for (int j=0; j < length; j++) {
+      OLED_update_buffer_single_byte(currentByte, (pgm_read_byte(&font5[font_table_index][j]));
+      //printf("currentByte: %d, asciivalue: %d\n\r", currentByte, pgm_read_byte(&font5[font_table_index][j]));
+      currentByte++;
+    }
+  }
+
+  // USERNAME
+  char* name;
+  unit8_t length;
+  switch(username){
+    case 0:
+      name = "Magne";
+      length = 5;
+      break;
+    case 1:
+      name = "Andrine";
+      length = 7;
+      break;
+    case 2:
+      name = "Thea";
+      length = 4;
+      break;
+  }
+
+  uint16_t b = 0;
+  while(name[b]) {
+    uint8_t currentChar = (uint8_t)name[b];
+    if(currentChar < 32){
+      continue;
+    }
+    uint8_t font_table_index = currentChar - 32;
+  for (int j=0; j < length; j++) {
+      OLED_update_buffer_single_byte(currentByte, (pgm_read_byte(&font5[font_table_index][j]));
+      //printf("currentByte: %d, asciivalue: %d\n\r", currentByte, pgm_read_byte(&font5[font_table_index][j]));
+      currentByte++;
+    }
+  }
+
+  char* score = (char*)(score_H << 8) + score_L;
+  uint16_t c = 0;
+  while(score[c]) {
+    uint8_t currentChar = (uint8_t)score[c];
+    if(currentChar < 32){
+      continue;
+    }
+    uint8_t font_table_index = currentChar - 32;
+  for (int j=0; j < length; j++) {
+      OLED_update_buffer_single_byte(currentByte, (pgm_read_byte(&font5[font_table_index][j]));
+      //printf("currentByte: %d, asciivalue: %d\n\r", currentByte, pgm_read_byte(&font5[font_table_index][j]));
+      currentByte++;
+    }
+  }
+}
+/*
+void print_highscore_node(uint8_t place, uint8_t username, uint8_t score_H, uint8_t score_L){
   printf("H");
   char data[128];
   data[0] = (char)place;
@@ -351,10 +429,10 @@ void print_highscore_node(uint8_t place, uint8_t username, uint8_t score_H, uint
   //data[length+3] = (char) score_L;
   /*for (int i = length+3; i < length+11; i++){
         data[i] = score_L;
-      }*/
+      }
   OLED_update_buffer_line(place, data);
 }
-
+*/
 /*
   MAPPING
   id = 3
